4fd507e13b2eeff12d6abed7fce96ef72dbbdaf7
Hegar Garcia <hegargarcia@gmail.com>
2019-11-23 11:41:50 -0600

    Adding HOC component for Auth Protected routes

--- hoc/withAuth.jsx
+++ hoc/withAuth.jsx
@@ -1,17 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import Router from "next/router";
 import nextCookie from "next-cookies";
-import cookie from "js-cookie";
-
-export const login = ({ token }) => {
-  cookie.set("token", token, { expires: 1 });
-  Router.push("/profile");
-};
+import PropTypes from "prop-types";
+import { AuthContext } from "../context/auth";
 
 export const auth = ctx => {
   const { token } = nextCookie(ctx);
 
-  // If there's no token, it means the user is not logged in.
   if (!token) {
     if (typeof window === "undefined") {
       ctx.res.writeHead(302, { Location: "/signin" });
@@ -24,31 +19,34 @@ export const auth = ctx => {
   return token;
 };
 
-export const logout = () => {
-  cookie.remove("token");
-  // to support logging out from all windows
-  window.localStorage.setItem("logout", Date.now());
-  Router.push("/login");
-};
-
 export const withAuth = WrappedComponent => {
-  const Wrapper = props => {
-    const syncLogout = event => {
+  const Wrapper = ({ token, ...props }) => {
+    const { signInWithToken, isValidToken } = useContext(AuthContext);
+
+    if (token && isValidToken(token)) {
+      signInWithToken(token);
+    }
+
+    const onSignOut = event => {
       if (event.key === "logout") {
         Router.push("/signin");
       }
     };
 
     useEffect(() => {
-      window.addEventListener("storage", syncLogout);
+      window.addEventListener("storage", onSignOut);
 
       return () => {
-        window.removeEventListener("storage", syncLogout);
+        window.removeEventListener("storage", onSignOut);
         window.localStorage.removeItem("logout");
       };
     }, []);
 
     return <WrappedComponent {...props} />;
+  };
+
+  Wrapper.propTypes = {
+    token: PropTypes.string.isRequired
   };
 
   Wrapper.getInitialProps = async ctx => {
